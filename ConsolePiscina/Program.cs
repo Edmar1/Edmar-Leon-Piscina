3dfafe795b7b1bb17eb897f9baa604f5304f650d
Edmar1 <edmarleta@gmail.com>
2025-08-28 09:40:38 -0500

    Solucion del conflicto

--- ConsolePiscina/Program.cs
+++ ConsolePiscina/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Conflicto");
+Console.WriteLine("Solucion del conflicto");
 public class Piscinas
 {
     public int Id;
