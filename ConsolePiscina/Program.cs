0972a9ff3001ae9047ba32621bdf38b895041288
Edmar1 <edmarleta@gmail.com>
2025-08-28 09:12:39 -0500

    Subida de archivos a la rama

--- ConsolePiscina/Program.cs
+++ ConsolePiscina/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Piscinas
+{
+    public int Id;
+    public string? Nombre;
+    public Socorristas? SocorristaAsignado; // Socorrista asignado a la piscina
+}
+
+public class Socorristas
+{
+    public int Id;
+    public string? Nombre;
+    public int Edad;
+    public string? Turno;
+}
+
+public class Clientes
+{
+    public int Id;
+    public string? Nombre;
+    public int Edad;
+}
+
+public class Reservas
+{
+    public List<Clientes> Clientes; // Lista de clientes en la reserva
+    public Piscinas Piscina; // Piscina asociada a la reserva
+    public int Id;
+    public DateTime Fecha;
+    public int DuracionHoras;
+}
